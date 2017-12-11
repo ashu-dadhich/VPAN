4da95fec1e1861b7ed613de2b9d662ba5bb1085f
Ashutosh Dadhich <ashutosh.dadhich@hotmail.com>
2017-12-11 15:52:02 +0530

    Base Android

---
+++ app/src/main/java/com/nit/myapplication/Constants.java
@@ -0,0 +1,11 @@
+package com.nit.myapplication;
+
+/**
+ * Created by ashu on 11/12/17.
+ */
+
+public class Constants {
+    public static final String msgSendURL="http://website220170713085214.azurewebsites.net/interact.ashx?ActivityPerformed=usertyped&Message=";
+    public static String respType="";
+    public static final String locationSendURL="";
+}
