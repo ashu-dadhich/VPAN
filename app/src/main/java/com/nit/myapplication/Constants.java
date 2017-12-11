f2ffc3417eb3cd88cf3092c17a90f69233b709b5
Ashutosh Dadhich <ashutosh.dadhich@hotmail.com>
2017-12-12 03:07:11 +0530

    New stable base

--- app/src/main/java/com/nit/myapplication/Constants.java
+++ app/src/main/java/com/nit/myapplication/Constants.java
@@ -5,7 +5,9 @@ package com.nit.myapplication;
  */
 
 public class Constants {
-    public static final String msgSendURL="http://website220170713085214.azurewebsites.net/interact.ashx?ActivityPerformed=usertyped&Message=";
-    public static String respType="";
-    public static final String locationSendURL="";
+    public static final String msgSendURL="https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/66c15449-c622-4e93-a138-a98bd1c34892/image?iterationId=80460a49-0ae8-421b-bdca-250a4956929f";
+    public static final String Prediction_Key="86e35977488c400c8faae2061640d6fb";
+    public static final String Content_Type="application/octet-stream";
+    public static String placeName="";
+    public static String prev_file_name="";
 }
--- app/src/main/java/com/nit/myapplication/MainActivity.java
+++ app/src/main/java/com/nit/myapplication/MainActivity.java
@@ -1,14 +1,38 @@
 package com.nit.myapplication;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import net.gotev.uploadservice.BinaryUploadRequest;
+import net.gotev.uploadservice.ServerResponse;
+import net.gotev.uploadservice.UploadInfo;
+import net.gotev.uploadservice.UploadNotificationConfig;
+import net.gotev.uploadservice.UploadStatusDelegate;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     private ListView mListView;
@@ -16,6 +40,12 @@ public class MainActivity extends AppCompatActivity {
     private EditText mEditTextMessage;
     private ImageView mImageView;
     private ChatMessageAdapter mAdapter;
+    //CAmera and File
+    final static private int NEW_PICTURE = 1;
+    private String mCameraFileName;
+    //MultiPart Upload
+    BinaryUploadRequest binaryUploadRequest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,12 +74,109 @@ public class MainActivity extends AppCompatActivity {
         ChatMessage chatMessage = new ChatMessage(message, true, false);
         mAdapter.add(chatMessage);
         //respond as Helloworld
-        mimicOtherMessage("HelloWorld");
+        if(message.equalsIgnoreCase("hi") || message.contains("hi") || message.equalsIgnoreCase("hello"))
+            mimicOtherMessage("Hi User");
+        else if(message.contains("help")||message.contains("Help"))
+            mimicCameraMessage("OK firing up Camera");
+        else
+            mimicOtherMessage("Sorry! I didn't catch that.");
     }
 
     private void mimicOtherMessage(String message) {
         ChatMessage chatMessage = new ChatMessage(message, false, false);
         mAdapter.add(chatMessage);
+    }
+
+    private void mimicCameraMessage(String message){
+        ChatMessage chatMessage = new ChatMessage(message, false, false);
+        mAdapter.add(chatMessage);
+        dispatchTakePictureIntent();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent intent = new Intent();
+        // Picture from camera
+        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // This is not the right way to do this, but for some reason, having
+        // it store it in
+        // MediaStore.Images.Media.EXTERNAL_CONTENT_URI isn't working right.
+
+        Date date = new Date();
+        DateFormat df = new SimpleDateFormat("-mm-ss");
+
+        String newPicFile = "Bild"+ df.format(date) + ".jpg";
+        String outPath = "/sdcard/" + newPicFile;
+        File outFile = new File(outPath);
+        Constants.prev_file_name=outPath;
+
+        mCameraFileName = outFile.toString();
+        Uri outuri = Uri.fromFile(outFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outuri);
+        startActivityForResult(intent, NEW_PICTURE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data)    {
+        if (requestCode == NEW_PICTURE){
+            // return from file upload
+            if (resultCode == Activity.RESULT_OK){
+                Uri uri = null;
+                if (data != null){
+                    uri = data.getData();
+                }if (uri == null && mCameraFileName != null){
+                    uri = Uri.fromFile(new File(mCameraFileName));
+                }
+                File file = new File(mCameraFileName);
+                if (!file.exists()) {
+                    file.mkdir();
+                }
+            }
+            try {
+                String uploadId = UUID.randomUUID().toString();
+                binaryUploadRequest=new BinaryUploadRequest(this,uploadId,Constants.msgSendURL);
+                binaryUploadRequest.setFileToUpload(Constants.prev_file_name);
+                binaryUploadRequest.addHeader("Prediction_Key",Constants.Prediction_Key);
+                binaryUploadRequest.addHeader("Content_Type",Constants.Content_Type);
+                binaryUploadRequest.setNotificationConfig(new UploadNotificationConfig());
+                binaryUploadRequest.setMaxRetries(2);
+                binaryUploadRequest.setDelegate(new UploadStatusDelegate() {
+                    @Override
+                    public void onProgress(UploadInfo uploadInfo) {
+                    }
+                    @Override
+                    public void onError(UploadInfo uploadInfo, Exception exception) {
+                    }
+                    @Override
+                    public void onCompleted(UploadInfo uploadInfo, ServerResponse serverResponse) {
+                        // Toast.makeText(getContext(), ""+serverResponse.getBodyAsString().toString(), Toast.LENGTH_SHORT).show();
+                        Log.d("ashu",serverResponse.getBodyAsString().toString());
+                        Log.d("ashu",serverResponse.toString());
+                        try {
+                            JSONObject jsonObject=new JSONObject(serverResponse.getBodyAsString());
+                            JSONArray jsonArray= jsonObject.getJSONArray("Predictions");
+                            for(int xi=0;xi<jsonArray.length();xi++){
+                                JSONObject temp=jsonArray.getJSONObject(xi);
+                                if(xi==0){
+                                    Constants.placeName=temp.getString("Tag");
+                                    Toast.makeText(MainActivity.this, ""+Constants.placeName, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(UploadInfo uploadInfo) {
+
+                    }
+                });
+                binaryUploadRequest.startUpload();
+            } catch (Exception exc) {
+                Toast.makeText(this, exc.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.d("ashu",Constants.prev_file_name+"error:"+exc.getMessage());
+            }
+
+        }
     }
 
     private void sendMessage() {
